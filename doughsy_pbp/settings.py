8ed11d7977968efc4b53426fb8432558b862c651
valizanady <149881195+valizanady@users.noreply.github.com>
2024-09-17 21:03:09 +0700

    Update settings.py

--- doughsy_pbp/settings.py
+++ doughsy_pbp/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://valiza-nadya-doughsy.pbp.cs.ui.ac.id", "https://valiza-nadya-doughsy.pbp.cs.ui.ac.id"]
